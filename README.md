bca41c58ed5b960ee5af950fde77e12947b22282
Collin Mutembei <mutembei.collin@gmail.com>
2016-01-28 17:10:34 +0300

    (feature) Add tests for API resources
    - Add tests for operations on Bucketlist and Items resources
    - Update README.md with details on running the project
    - Refactor the `travis.yml` config file to rn coverage
    - Update requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 [![Build Status](https://travis-ci.org/andela-cmutembei/III.svg)](https://travis-ci.org/andela-cmutembei/III)
 [![Coverage Status](https://coveralls.io/repos/andela-cmutembei/III/badge.svg?branch=develop&service=github)](https://coveralls.io/github/andela-cmutembei/III?branch=develop)
 [![Code Issues](https://www.quantifiedcode.com/api/v1/project/f3b027bfc00949219f46c6aa0cf5da3a/snapshot/origin:develop:HEAD/badge.svg)](https://www.quantifiedcode.com/app/project/f3b027bfc00949219f46c6aa0cf5da3a)
+
+## BLST
+BLST is a RESTful API service for managing bucket lists and their constituent items. It is built using Flask and uses JSON objects for information interchange.
+
+#### Project requirements
+- [Python](https://www.python.org/downloads/)
+- [Virtualenvwrapper](https://virtualenvwrapper.readthedocs.org/en/latest/install.html)
+- [Postgresql](http://www.postgresql.org/download/)
+
+#### Documentation
+The API documentation can be found [here](https://blst-api.herokuapp.com/docs)
+#### Installation
+To run blst locally configure [environment variables](https://github.com/andela-cmutembei/III/wiki) and do the following:
+```shell
+$ git clone https://github.com/andela-cmutembei/III.git && cd $_
+
+$ workon III-env
+
+(III-env)$ pip install -r requirements.txt
+
+(III-env)$ python manage.py migrate
+
+(III-env)$ python manage.py runserver
+```
+
+#### Running tests
+To run unit tests for blst
+```shell
+(bucketlist-env)$ python manage.py test
+```
---
+++ tests/test_api_resources.py
@@ -0,0 +1,82 @@
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+
+
+class APIResourcesTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('tester', password='test')
+        self.user.save()
+        token_response = self.client.post('/auth/login/', {'username': 'tester', 'password': 'test'})
+
+        self.client = APIClient()
+        self.token = token_response.data['token']
+
+    def test_bucketlists_resource_requires_auth(self):
+        post_response = self.client.post('/bucketlists/', {'name': 'test bucketlist'})
+        get_response = self.client.get('/bucketlists/')
+
+        self.assertEqual(post_response.status_code, 403)
+        self.assertEqual(get_response.status_code, 403)
+
+    def test_get_bucketlists_when_autheticated(self):
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        response = self.client.get('/bucketlists/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [])
+
+    def test_crud_operations_on_bucketlist_when_autheticated(self):
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        response = self.client.post('/bucketlists/', {'name': 'test bucketlist'})
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'test bucketlist')
+
+        bucketlist_id = response.data['id']
+
+        # Test getting single bucketlists
+        single_bucketlist = self.client.get('/bucketlists/' + str(bucketlist_id) + '/')
+
+        self.assertEqual(single_bucketlist.status_code, 200)
+        self.assertEqual(single_bucketlist.data['id'], bucketlist_id)
+
+        # Test updating bucketlist
+        updated_bucketlist = self.client.put('/bucketlists/' + str(bucketlist_id) + '/', {'name': 'new bucketlist'})
+
+        self.assertEqual(updated_bucketlist.status_code, 200)
+        self.assertEqual(updated_bucketlist.data['name'], 'new bucketlist')
+
+        # Test deleting bucketlist
+        another_item = self.client.post('/bucketlists/' + str(bucketlist_id) + '/items/', {'name': 'another item', 'parent_bucketlist': bucketlist_id})
+        delete_bucketlist = self.client.delete('/bucketlists/' + str(bucketlist_id) + '/')
+        bucketlist_item = self.client.get('/bucketlists/' + str(bucketlist_id) + '/items/' + str(another_item.data['id']) + '/')
+
+        self.assertEqual(delete_bucketlist.status_code, 204)
+        self.assertEqual(bucketlist_item.status_code, 404)
+
+    def test_crud_operations_on_item_in_bucketlist_when_authenticated(self):
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        another_bucketlist = self.client.post('/bucketlists/', {'name': 'another test bucketlist'})
+        bucketlist_id = another_bucketlist.data['id']
+
+        # Creating item in bucketlist
+        response = self.client.post('/bucketlists/' + str(bucketlist_id) + '/items/', {'name': 'test item', 'parent_bucketlist': bucketlist_id})
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['name'], 'test item')
+
+        item_id = response.data['id']
+
+        # Test getting added item
+        created_item = self.client.get('/bucketlists/' + str(bucketlist_id) + '/items/' + str(item_id) + '/')
+        # import ipdb; ipdb.set_trace()
+
+        self.assertEqual(created_item.status_code, 200)
+        self.assertEqual(created_item.data['id'], item_id)
+
+        # Test updating item
+        updated_item = self.client.put('/bucketlists/' + str(bucketlist_id) + '/items/' + str(item_id) + '/', {'name': 'new item', 'parent_bucketlist': bucketlist_id})
+
+        self.assertEqual(updated_item.status_code, 200)
+        self.assertEqual(updated_item.data['name'], 'new item')
